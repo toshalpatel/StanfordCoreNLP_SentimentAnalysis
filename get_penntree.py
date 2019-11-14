59857812ebe18472d523f17a9e4006afaf3df32f
Toshal Patel <toshaltpatel@gmail.com>
2019-11-14 21:33:43 +0800

    get penntree representation of the sentences
    Uses the standard model to calculate sentiment scores for words, phrases and sentences.

---
+++ get_penntree.py
@@ -0,0 +1,38 @@
+import nltk
+import re
+import pandas as pd
+from pycorenlp import StanfordCoreNLP
+import sys
+import time
+import subprocess
+
+def get_clean_text(text):
+    fstr = re.sub("[^A-Za-z.']+", ' ', text)
+    return fstr
+
+data = pd.read_csv("~/oh_textmining/combined_sentences_141119.csv")
+
+def get_pennt(text):
+    text_file = open("eg.txt", "w")
+    text_file.write(text)
+    text_file.close()
+    proc = subprocess.run("java -mx4g -cp \"/NAS/home01/toshal/stanford-corenlp-full-2018-10-05/*\" edu.stanford.nlp.sentiment.SentimentPipeline -file eg.txt -output PENNTREES ", stdout=subprocess.PIPE, shell=True)
+    #proc.stdin.write(text)
+    #f = open("demofile.txt", "r")
+    #print(proc.communicate())
+    pennt = proc.stdout
+    result = re.search('\n(.*)\n', pennt.decode("utf-8"))
+    pennt=result.group(1)
+    print(pennt)
+    return pennt
+    
+    
+time.sleep(2)
+
+data['penn_t'] = data['text'].apply(lambda row:get_clean_text(row))
+
+for i in data.index:
+    data.loc[i,'penn_t'] = get_pennt(data.loc[i,'text'])
+    
+print(data['penn_t'])
+data.to_csv("combined_sentences_141119_with_penn.csv", index=None)
